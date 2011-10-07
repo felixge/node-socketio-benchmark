6d950ec9aa8734a5815c5b85a2a7065b9d5cfa60
Robin Mehner <robin@coding-robin.de>
2011-10-07 14:16:13 +0200

    Better error-handling for websocket client

--- transport/websocket.js
+++ transport/websocket.js
@@ -30,17 +30,21 @@ WebSocket.prototype.connect = function() {
     {'force new connection': true}
   );
 
+  this.connection.on('error', this.handleError.bind(this));
+  this.connection.socket.on('error', this.handleError.bind(this));
+
   this.connection.on('connection', function() {
     self.emit('connection');
-  });
-
-  this.connection.on('error', function(error) {
-    self.emit('error', error);
   });
 
   this.connection.on('chat.msg', function(message) {
     self.emit('message', message);
   });
+}
+
+WebSocket.prototype.handleError = function(error) {
+  var err = typeof error === 'string' ? new Error(error) : error;
+  this.emit('error', err);
 }
 
 WebSocket.prototype.send = function(message) {
