55ab30fd4cf7b7aabddd18b2ee71756f05316155
Robin Mehner <robin@coding-robin.de>
2011-10-07 11:00:26 +0200

    Grab changes from the other repo

--- websocket_client.js
+++ websocket_client.js
@@ -1,5 +1,6 @@
 var program = require('commander');
-var io      = require('socket.io-client')
+var io      = require('socket.io-client');
+var clients = [];
 
 program
   .option('-p, --port [port]', 'The port to connect to [3030]', 3030)
@@ -9,17 +10,27 @@ program
   .parse(process.argv);
 
 function WebsocketClient() {
-  this.connection = undefined;
+  this.connection       = null;
+  this.receivedMessages = 0;
 }
 
 WebsocketClient.prototype.connect = function() {
-  this.connection = io.connect('http://' + program.host + ':' + program.port);
+  this.connection = io.connect(
+    'http://' + program.host + ':' + program.port,
+    {'force new connection': true}
+  );
+
+  this.connection.on('chat.msg', this.handleChatMessage.bind(this));
 }
 
 WebsocketClient.prototype.sendMsg = function() {
   if (this.connection.socket.connected) {
     this.connection.emit('chat.send', {msg: 'Your mum'});
   }
+}
+
+WebsocketClient.prototype.handleChatMessage = function(data) {
+  this.receivedMessages++;
 }
 
 WebsocketClient.prototype.sendMessages = function() {
@@ -33,4 +44,21 @@ for (var i = 0; i < program.clients; i++) {
   var client = new WebsocketClient();
   client.connect();
   client.sendMessages();
+  clients.push(client);
 }
+
+setInterval(function() {
+  var sum              = 0;
+  var receivedMessages = clients.map(function(client) {
+    var messageCount = client.receivedMessages;
+    client.receivedMessages = 0;
+
+    return messageCount;
+  });
+
+  receivedMessages.forEach(function(count) {
+    sum += count;
+  });
+
+  console.log('Received %d requests per second', sum / 10000 * 1000)
+}, 10000);
